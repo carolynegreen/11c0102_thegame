7cc33ab7842c7a892fe6329f6ca3f2371477527d
carolynegreen <carolynemarie94@gmail.com>
2021-09-07 12:30:29 +0200

    updated something

--- script.js
+++ script.js
@@ -1,3 +1,37 @@
 "use strict";
-const player = document.querySelector("#player1");
-const computer = document.querySelector("#player2");
+const player = document.getElementById("player1");
+const computer = document.getElementById("player2");
+const results = document.getElementById("texts");
+const possibleChoices = document.querySelectorAll("button");
+let userChoice;
+let computerChoice;
+
+//get user choice
+possibleChoices.forEach((possibleChoice) =>
+  possibleChoice.addEventListener("click", (e) => {
+    userChoice = e.target.id;
+    console.log(possibleChoice);
+    generateComputerChoice();
+    getResults();
+  })
+);
+
+//get computer choice
+function generateComputerChoice() {
+  const randomNumber = Math.floor(Math.random() * possibleChoices.length);
+  if (randomNumber === 0) {
+    computerChoice = "rock";
+  }
+  if (randomNumber === 1) {
+    computerChoice = "paper";
+  }
+  if (randomNumber === 2) {
+    computerChoice = "scissors";
+  }
+  console.log(computerChoice);
+}
+
+function getResults() {
+  if (computerChoice === userChoice) {
+  }
+}
